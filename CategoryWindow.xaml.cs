916110ade36e1aac03c923aaa0a9f7e115dcdca9
Nekit951 <Mdvdvnkt@gmail.com>
2026-06-18 12:39:24 +0300

    Add files via upload

---
+++ CategoryWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WpfApp3.Contexts;
+using WpfApp3.Models;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Логика взаимодействия для CategoryWindow.xaml
+    /// </summary>
+    public partial class CategoryWindow : Window
+    {
+        private string _role;
+        public CategoryWindow(string role)
+        {
+            InitializeComponent();
+            _role = role;
+            LoadCategory();
+            aaa();
+        }
+
+        void aaa()
+        {
+            if(_role == "admin")
+            {
+                InputCategory.Visibility = Visibility.Visible;
+                AddCategory.Visibility = Visibility.Visible;
+                DelCategory.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                InputCategory.Visibility = Visibility.Collapsed;
+                AddCategory.Visibility = Visibility.Collapsed;
+                DelCategory.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void LoadCategory()
+        {
+            MyContext myContext = new MyContext();
+            dataGrid.ItemsSource = myContext.Categories.ToList();
+        }
+
+        private void AddCategory_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MyContext myContext = new MyContext();
+                Category category = new Category();
+                category.categoryName = InputCategory.Text;
+                myContext.Add(category);
+                myContext.SaveChanges();
+                LoadCategory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void DelCategory_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MyContext myContext = new MyContext();
+                myContext.Categories.Remove(dataGrid.SelectedItem as Category);
+                myContext.SaveChanges();
+                LoadCategory();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _role;
+        public MainWindow(string role)
+        {
+            InitializeComponent();
+            _role = role;
+        }
+
+        private void TovarButton_Click(object sender, RoutedEventArgs e)
+        {
+            TovarWindow tovarWindow = new TovarWindow(_role);
+            tovarWindow.Show();
+        }
+
+        private void CategoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            CategoryWindow categoryWindow = new CategoryWindow(_role);
+            categoryWindow.Show();
+        }
+
+        private void UserButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserWindow userWindow = new UserWindow(_role);
+            userWindow.Show();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            OrderWindow orderWindow = new OrderWindow(_role);
+            orderWindow.Show();
+        }
+    }
+}
